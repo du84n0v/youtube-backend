b881e6369e1a69e70d3f6fda2d06ef67dfe4a822
ziyodullayevj7 <ziyodullayevj7@gmail.com>
2026-06-24 12:46:55 +0500

    Category entity fully added

---
+++ src/main/java/com/youtube/entity/CategoryEntity.java
@@ -0,0 +1,25 @@
+package com.youtube.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_date_time")
+    private LocalDateTime createdDateTime;
+}
