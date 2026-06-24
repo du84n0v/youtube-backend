8cea2d28b96aac1c5d3dd9c4587b0ffc04087397
teo <azamaturmanov822@gmail.com>
2026-06-24 16:39:35 +0500

    attach

---
+++ src/main/java/com/youtube/repository/AttachRepository.java
@@ -0,0 +1,18 @@
+package com.youtube.repository;
+
+import com.youtube.entity.AttachEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttachRepository extends CrudRepository<AttachEntity,String> {
+
+
+    @Query("from AttachEntity order by createdDateTime desc")
+    Page<AttachEntity> findAllByOrderByCreatedDateDesc(Pageable pageable);
+
+
+}
