30ada754e96f7731740f7191c4348d8395a5d018
ziyodullayevj7 <ziyodullayevj7@gmail.com>
2026-06-24 13:34:30 +0500

    Playlist entity fully added with PlaylistStatusEnum

---
+++ src/main/java/com/youtube/entity/PlaylistEntity.java
@@ -0,0 +1,35 @@
+package com.youtube.entity;
+
+import com.youtube.enums.PlaylistStatusEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "playlist")
+@Getter
+@Setter
+public class PlaylistEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "channel_id")
+    private String channelId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "channel_id", insertable = false, updatable = false)
+    private ChannelEntity channel;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private PlaylistStatusEnum status;
+
+    @Column(name = "order_num")
+    private Integer orderNumber;
+}
---
+++ src/main/java/com/youtube/enums/PlaylistStatusEnum.java
@@ -0,0 +1,5 @@
+package com.youtube.enums;
+
+public enum PlaylistStatusEnum {
+    PUBLIC, PRIVATE
+}
