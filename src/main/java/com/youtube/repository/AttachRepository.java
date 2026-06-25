fcc1525281c800e5155bfef917fbd70832315a44
teo <azamaturmanov822@gmail.com>
2026-06-25 14:39:28 +0500

    changed AttachService -> toDTO-> fully working without errors(entity.getCreatedDateTime to entity.getCreatedDate)

--- src/main/java/com/youtube/repository/AttachRepository.java
+++ src/main/java/com/youtube/repository/AttachRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AttachRepository extends CrudRepository<AttachEntity,String> {
 
 
