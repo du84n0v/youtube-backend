4e9fe14c3ced07454c449a5358657ac8a6e2e84b
sardornurmatov815-hash <sardornurmatov815@gmail.com>
2026-06-27 16:53:15 +0500

    VideoTagRepositoryga ozgartirish kiritildi

--- src/main/java/com/youtube/repository/VideoTagRepository.java
+++ src/main/java/com/youtube/repository/VideoTagRepository.java
@@ -2,16 +2,28 @@ package com.youtube.repository;
 
 import com.youtube.entity.VideoTagEntity;
 import org.hibernate.internal.util.Optional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface VideoTagRepository extends CrudRepository<VideoTagEntity, Integer> {
-    boolean existsByVideoIdAndTagId(Integer videoId, Integer tagId);
+    @Query("SELECT CASE WHEN COUNT(v) > 0 THEN true ELSE false END FROM VideoTagEntity v WHERE v.videoId = :videoId AND v.tagId = :tagId")
+    boolean existsCheck(String videoId, Integer tagId);
 
     List<VideoTagEntity> findAllByVideoId(Integer videoId);
 
-    Optional<VideoTagEntity>  findByVideoIdAndTagId(Integer videoId, Integer tagId);
 
-    void deleteByVideoIdAndTagId(Integer videoId, Integer tagId);
+    <T> Optional<T> findByVideoIdAndTagId(String videoId, Integer tagId, Class<T> type);
+
+
+    @Query("DELETE FROM VideoTagEntity v WHERE v.videoId = :videoId AND v.tagId = :tagId")
+    @Modifying
+    @Transactional
+    void deleteByVideoIdAndTagId(String videoId, Integer tagId);
+
+
+
 }
