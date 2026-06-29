5c9329f2ed7b8f4b52fc52fd685381a961b9fa9d
dusanov <sadriddindusanov@gmail.com>
2026-06-29 21:44:54 +0500

    email history filter api is done but not tested

--- src/main/java/com/youtube/controller/EmailHistoryController.java
+++ src/main/java/com/youtube/controller/EmailHistoryController.java
@@ -1,6 +1,7 @@
 package com.youtube.controller;
 
 import com.youtube.dto.emailhistory.EmailHistoryDTO;
+import com.youtube.dto.emailhistory.EmailHistoryFilterDTO;
 import com.youtube.service.EmailHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,15 +19,24 @@ public class EmailHistoryController {
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("")
     public ResponseEntity<Page<EmailHistoryDTO>> getList(@RequestParam(name = "page", defaultValue = "1") Integer page,
-                                                         @RequestParam(name = "size", defaultValue = "20") Integer size){
-        return ResponseEntity.ok(historyService.getList(page-1, size));
+                                                         @RequestParam(name = "size", defaultValue = "20") Integer size) {
+        return ResponseEntity.ok(historyService.getList(page - 1, size));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/by-email")
     public ResponseEntity<Page<EmailHistoryDTO>> getProfileHistory(@RequestParam("email") String email,
                                                                    @RequestParam(name = "page", defaultValue = "1") Integer page,
-                                                                   @RequestParam(name = "size", defaultValue = "20") Integer size){
-        return ResponseEntity.ok(historyService.getProfileHistory(email, page-1, size));
+                                                                   @RequestParam(name = "size", defaultValue = "20") Integer size) {
+        return ResponseEntity.ok(historyService.getProfileHistory(email, page - 1, size));
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/filter")
+    public ResponseEntity<Page<EmailHistoryDTO>> filter(@RequestParam(name = "page", defaultValue = "1") Integer page,
+                                                        @RequestParam(name = "size", defaultValue = "10") Integer size,
+                                                        @RequestBody EmailHistoryFilterDTO dto) {
+        return ResponseEntity.ok(historyService.filter(dto, page - 1, size));
+    }
+
 }
--- src/main/java/com/youtube/service/EmailHistoryService.java
+++ src/main/java/com/youtube/service/EmailHistoryService.java
@@ -2,9 +2,11 @@ package com.youtube.service;
 
 import com.youtube.dto.auth.MailMessageDTO;
 import com.youtube.dto.emailhistory.EmailHistoryDTO;
+import com.youtube.dto.emailhistory.EmailHistoryFilterDTO;
 import com.youtube.entity.EmailHistoryEntity;
 import com.youtube.enums.EmailCodeTypeEnum;
 import com.youtube.repository.EmailHistoryRepository;
+import com.youtube.repository.custom.CustomEmailHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -19,6 +21,8 @@ public class EmailHistoryService {
 
     @Autowired
     private EmailHistoryRepository historyRepository;
+    @Autowired
+    private CustomEmailHistoryRepository customHistoryRepository;
 
     public void create(MailMessageDTO dto, String code, EmailCodeTypeEnum type) {
         EmailHistoryEntity history = new EmailHistoryEntity();
@@ -43,11 +47,14 @@ public class EmailHistoryService {
         Pageable pageable = PageRequest.of(page, size);
         Page<EmailHistoryEntity> result = historyRepository.findAll(pageable);
 
-        return new PageImpl<>(result.stream().map(this::entityToDto).toList(), pageable, result.getTotalElements());
+        return new PageImpl<>(
+                result.stream().map(this::entityToDto).toList(),
+                pageable,
+                result.getTotalElements());
 
     }
 
-    private EmailHistoryDTO entityToDto(EmailHistoryEntity entity){
+    private EmailHistoryDTO entityToDto(EmailHistoryEntity entity) {
         EmailHistoryDTO dto = new EmailHistoryDTO();
         dto.setId(entity.getId());
         dto.setToEmail(entity.getToEmail());
@@ -63,6 +70,19 @@ public class EmailHistoryService {
         Pageable pageable = PageRequest.of(page, size);
         Page<EmailHistoryEntity> pages = historyRepository.getAllByToEmail(email, pageable);
 
-        return new PageImpl<>(pages.stream().map(this::entityToDto).toList(), pageable, pages.getTotalElements());
+        return new PageImpl<>(
+                pages.stream().map(this::entityToDto).toList(),
+                pageable,
+                pages.getTotalElements());
+    }
+
+    public Page<EmailHistoryDTO> filter(EmailHistoryFilterDTO dto, Integer page, Integer size) {
+        Page<EmailHistoryEntity> pages = customHistoryRepository.filter(dto, page, size);
+
+        return new PageImpl<>(
+                pages.stream().map(this::entityToDto).toList(),
+                PageRequest.of(page, size),
+                pages.getTotalElements()
+        );
     }
 }
