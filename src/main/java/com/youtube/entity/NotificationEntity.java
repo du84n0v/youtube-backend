4fa4e2f0f97e646af2b7517464a8decabe4cca36
sardornurmatov815-hash <sardornurmatov815@gmail.com>
2026-07-02 00:23:34 +0500

    NotificationEntity yozildi

---
+++ src/main/java/com/youtube/entity/NotificationEntity.java
@@ -0,0 +1,50 @@
+package com.youtube.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notification")
+@Getter
+@Setter
+public class NotificationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "profile_id")
+    private Integer profileId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", insertable = false,updatable = false)
+    private ProfileEntity profile;
+
+    @Column(name = "channel_id")
+    private String channelId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "channel_id", insertable = false,updatable = false)
+    private ChannelEntity channel;
+
+    @Column(name = "video_id")
+    private String videoId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "video_id" , insertable = false, updatable = false)
+    private VideoEntity video;
+
+    @Column(name ="message" , columnDefinition = "text")
+    private String message;
+
+    @Column(name = "is_received")
+    private Boolean isReceived = false;
+
+    @Column(name = "created_date")
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+
+}
